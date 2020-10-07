338db1eb00620e963601644d07de924507438953
JustinJnkns <69018193+JustinJnkns@users.noreply.github.com>
2020-10-07 04:54:58 -0400

    added html generation

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-
-const printProfileData = profileDataArr => {
-  //this...
-    for (let i = 0; i < profileDataArr.length; i++) {
-     console.log(profileDataArr[i]);
-    }
-    console.log('===============')
-     // Is the same as this...
-     profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
-  printProfileData(profileDataArgs);
-  
+//initiats FS a module in node.JS
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+//holds user command-line arguements
+const profileDataArgs = process.argv.slice(2)
+//variables for extracted args
+const [name, github] = profileDataArgs;
+  //arguement functions for writeFile are name of file being created,data that will write into file,callback function used for error handling 
+fs.writeFile('index.html',generatePage(name,github),err=> {
+  if (err) throw new Error(err)
+  console.log('Portfolio complete! check out index.html to see output')
+})
