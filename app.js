2f82c59e81cf3928d35567c129162ec18ee5497d
JustinJnkns <69018193+JustinJnkns@users.noreply.github.com>
2020-10-10 22:28:29 -0400

    created questions for the github project

--- app.js
+++ app.js
@@ -13,16 +13,82 @@ const inquirer = require('inquirer')
 // })
 
 
-
-//mesage is question, Name is answer label 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      //Name: + user answer
-      name: 'Name',
-      //question
-      message: 'What is your name'
+const promptProject = portfolioData => {
+        //if no 'projects' array property, create one
+        if (!portfolioData.projects) {
+            portfolioData.projects = []
+        }
+        console.log('ADD A NEW PROJECT')
+        return inquirer.prompt([{
+                    type: 'input',
+                    name: 'name',
+                    message: 'what is the name of your project'
+                },
+                {
+                    type: 'input',
+                    name: 'description',
+                    message: 'Provide a description for the project(Required)'
+                },
+                {
+                    type: 'checkbox',
+                    name: 'languages',
+                    message: 'What did you build this project with?(check all that apply)',
+                    choices: ['Javascript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Boostrap']
+                },
+                {
+                    type: 'input',
+                    name: 'link',
+                    message: 'Enter link to Github Project. (Required)'
+                },
+                {
+                    type: 'confirm',
+                    name: 'feature',
+                    message: 'Would you like to feature this project?',
+                    default: false
+                },
+                {
+                    type: 'confirm',
+                    name: 'confirmAddProject',
+                    message: 'Would you like to enter another project?',
+                    default: false
+                }
+            ])
+            //adds project data to projects arra
+            .then(projectData => {
+                portfolioData.projects.push(projectData)
+                    //if user confirms addProject  calls the promptproject function 
+                if (projectData.confirmAddProject) {
+                    return promptProject(portfolioData)
+                }
+                //if user decides not to it evalutes to be false and returns portfolioData,important to return to retrieve users answers
+                else {
+                    return portfolioData
+                }
+            })
     }
-  ])
-    .then(answers => console.log(answers))
+    //mesage is question, Name is answer label 
+const promptUser = () => {
+    return inquirer.prompt([{
+            type: 'input',
+            //Name: + user answer
+            name: 'Name',
+            //question
+            message: 'What is your name'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your Github Name'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself'
+        }
+    ])
+}
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        console.log(portfolioData)
+    })
